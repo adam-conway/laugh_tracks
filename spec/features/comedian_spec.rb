63636c3b0deb315ced0dbc223f5c1772ac1a4a3f
adam-conway <adam.n.conway@gmail.com>
2018-03-16 11:00:03 -0600

    Added ability to count the number of specials for a comedian

--- spec/features/comedian_spec.rb
+++ spec/features/comedian_spec.rb
@@ -1,4 +1,4 @@
-describe 'User' do
+RSpec.describe 'User' do
   describe 'User goes to comedians page' do
     it 'Sees all comedians on comedians page' do
       Comedian.create(name: 'Adam', age: 26)
@@ -24,6 +24,26 @@ describe 'User' do
 
       expect(page).to have_content('Average age')
       expect(page).to have_content(27)
+    end
+    it 'Sees comedians with specific age' do
+      comedian1 = Comedian.create(name: 'Adam', age: 26)
+      comedian2 = Comedian.create(name: 'Evan', age: 34)
+
+      visit `/comedians?age=34`
+
+      expect(page).to have_content('Evan')
+      expect(page).to have_content(34)
+      expect(page).not_to have_content('Adam')
+      expect(page).not_to have_content(26)
+    end
+    it 'Sees count of comedians specials' do
+      comedian = Comedian.create(name: 'Adam', age: 26)
+      Comedian.create(name: 'Evan', age: 28)
+      Special.create(name: 'Special!', comedian_id: comedian.id)
+
+      visit '/comedians'
+
+      expect(page).to have_content('Number of specials: 1')
     end
   end
 end
