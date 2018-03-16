4a83a07cc1078359ab4051c41a1369ff3782cc32
adam-conway <adam.n.conway@gmail.com>
2018-03-16 10:30:35 -0600

    Added test for seeing specials on the comedians page

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -1,12 +1,20 @@
 RSpec.describe 'User' do
   describe 'User goes to comedians page' do
-    it 'Sees all users on user page' do
+    it 'Sees all comedians on comedians page' do
       Comedian.create(name: 'Adam', age: 26)
 
       visit '/comedians'
 
       expect(page).to have_content('Adam')
       expect(page).to have_content(26)
+    end
+    it 'Sees a comedians specials' do
+      comedian = Comedian.create(name: 'Adam', age: 26)
+      Special.create(name: 'Special!', comedian_id: comedian.id)
+
+      visit '/comedians'
+
+      expect(page).to have_content('Special!')
     end
   end
 end
