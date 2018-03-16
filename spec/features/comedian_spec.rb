620f2fbeb4363d26f4d098dd4fbeb62c111090fc
adam-conway <adam.n.conway@gmail.com>
2018-03-16 10:48:33 -0600

    Added ability to display average age

--- spec/features/comedian_spec.rb
+++ spec/features/comedian_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe 'User' do
+describe 'User' do
   describe 'User goes to comedians page' do
     it 'Sees all comedians on comedians page' do
       Comedian.create(name: 'Adam', age: 26)
@@ -15,6 +15,15 @@ RSpec.describe 'User' do
       visit '/comedians'
 
       expect(page).to have_content('Special!')
+    end
+    it 'Sees all comedians average age' do
+      comedian1 = Comedian.create(name: 'Adam', age: 26)
+      comedian2 = Comedian.create(name: 'Evan', age: 28)
+
+      visit '/comedians'
+
+      expect(page).to have_content('Average age')
+      expect(page).to have_content(27)
     end
   end
 end
